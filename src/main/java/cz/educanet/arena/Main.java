d913cdfec38c9ec25756a82883a0fee32acda7f5
vladimir-simek <minervladik@gmail.com>
2021-03-16 19:55:29 +0100

    Gladiator.java done

--- src/main/java/cz/educanet/arena/Main.java
+++ src/main/java/cz/educanet/arena/Main.java
@@ -2,6 +2,8 @@ package cz.educanet.arena;
 
 import cz.educanet.arena.presentation.ArenaCLI;
 
+import java.util.Random;
+
 /**
  * Main entry point
  */
@@ -9,6 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         ArenaCLI cli = new ArenaCLI();
+        cli.init();
         cli.renderLandingPage();
         cli.renderGladiatorPicker();
 
