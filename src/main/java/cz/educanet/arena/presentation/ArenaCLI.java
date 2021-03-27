b7cee1631e7963a751d6ebf860a7384d11fe84a6
vladimir-simek <minervladik@gmail.com>
2021-03-27 20:30:23 +0100

    Gladiator creator in progress

--- src/main/java/cz/educanet/arena/presentation/ArenaCLI.java
+++ src/main/java/cz/educanet/arena/presentation/ArenaCLI.java
@@ -4,6 +4,7 @@ import cz.educanet.arena.data.GladiatorDatabase;
 import cz.educanet.arena.logic.Arena;
 import cz.educanet.arena.logic.Gladiator;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ArenaCLI {
@@ -30,18 +31,43 @@ public class ArenaCLI {
     }
 
     public void renderGladiatorPicker() {
-        System.out.println("Do you wish to create a new Gladiator or load already existing one?");
+        System.out.println("Do you wish to create a new Gladiators or load already existing Gladiators?");
         System.out.println("1/2");
-        int option = sc.nextInt();
-        System.out.println("Pick your fighter #1 (enter the path of the saved file):");
-        String gladiator1File = sc.nextLine();
-        System.out.println("Pick your fighter #2 (enter the path of the saved file):");
-        String gladiator2File = sc.nextLine();
+        String rawOption = sc.nextLine();
+        int option = Integer.parseInt(rawOption);
 
-        Gladiator gladiator1 = GladiatorDatabase.loadGladiator(gladiator1File);
-        Gladiator gladiator2 = GladiatorDatabase.loadGladiator(gladiator2File);
-        logic.setGladiator1(gladiator1);
-        logic.setGladiator2(gladiator2);
+        try {
+            while (option != 1 && option != 2) {
+                rawOption = sc.nextLine();
+                option = Integer.parseInt(rawOption);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (option == 2) {
+
+            System.out.println("Pick your fighter #1 (enter the path of the saved file):");
+            String gladiator1File = sc.nextLine();
+            System.out.println("Pick your fighter #2 (enter the path of the saved file):");
+            String gladiator2File = sc.nextLine();
+
+
+            Gladiator gladiator1 = GladiatorDatabase.loadGladiator(gladiator1File);
+            Gladiator gladiator2 = GladiatorDatabase.loadGladiator(gladiator2File);
+            logic.setGladiator1(gladiator1);
+            logic.setGladiator2(gladiator2);
+        } else {
+            Gladiator gladiator1 = new Gladiator();
+            System.out.println("Enter Gladiators number 1 name:");
+            gladiator1.setName(sc.nextLine());
+            gladiator1.setHp(100);
+            gladiator1.setMinDMG(10);
+            gladiator1.setMaxDMG(20);
+
+            System.out.println("Enter Gladiators number 1 name:");
+
+        }
     }
 
     /**
